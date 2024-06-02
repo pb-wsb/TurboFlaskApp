5da6ba6c5ba8f4d2029c3bb7c1fb0f2fc890cb40
vdi-terminal <vdi-terminal@EDU-WSB.local>
2024-06-02 15:02:08 +0200

    init

---
+++ flask_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from FlaskApp.TurboFlaskApp.app import app
+
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+
+# test_app.py
+def test_home(client):
+    response = client.get('/')
+    response2 = client.get('/home')
+    assert response.status_code == 200
+    assert response2.status_code == 200
+
